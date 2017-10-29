8cdb9667653972ed95f5d58b0d46ed33c66de0e8
Henrik Mikkelsen <henrik.mikkelsen@ppmanager.dk>
2017-10-29 19:02:06 +0100

    Update select form groups

--- resources/assets/js/components/forms/horizontal/SelectFormGroup.vue
+++ resources/assets/js/components/forms/horizontal/SelectFormGroup.vue
@@ -1,9 +1,26 @@
 <template>
     <form-group :name="name" :label="label" :error="error" :label-width="labelWidth" :input-width="inputWidth">
         <select class="form-control" :id="name" :value="value" @input="$emit('input', $event.target.value)">
-            <option value="" :disabled="!nullable">{{ nullable ? '(none)' : 'Choose...' }}</option>
+            <option value="" :disabled="!nullable">
+                {{ nullable ? '(none)' : 'Choose...' }}
+            </option>
 
-            <option v-for="option in options" :value="option.value">{{ option.label }}</option>
+            <option v-for="option in options" :value="option.value" v-if="!hasOptionGroups">
+                {{ option.label }}
+            </option>
+
+            <optgroup
+                    v-for="(optionGroupOptions, optionGroupName) in options"
+                    :label="optionGroupName | readable"
+                    v-if="hasOptionGroups && optionGroupOptions.length"
+            >
+                <option
+                        v-for="option in optionGroupOptions"
+                        :value="getOptionGroupValue(optionGroupName, option.value)"
+                >
+                    {{ option.label }}
+                </option>
+            </optgroup>
         </select>
     </form-group>
 </template>
@@ -14,6 +31,26 @@
     export default {
         components: {FormGroup},
 
-        props: ["name", "label", "value", "error", "labelWidth", "inputWidth", "options", "nullable"]
+        props: ["name", "label", "value", "error", "labelWidth", "inputWidth", "options", "nullable"],
+
+        computed: {
+            hasOptionGroups() {
+                return !(this.options instanceof Array);
+            }
+        },
+
+        filters: {
+            readable(string) {
+                return string.split("_").map(function(word) {
+                    return word.replace(word[0], word[0].toUpperCase());
+                }).join(" ");
+            }
+        },
+
+        methods: {
+            getOptionGroupValue(optionGroupName, optionValue) {
+                return optionGroupName.replace("_", "-") + "-" + optionValue;
+            }
+        }
     }
 </script>
