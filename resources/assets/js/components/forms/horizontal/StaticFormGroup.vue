293a973c425ea80cc2e4208df122cdd96b1d75cd
Henrik Mikkelsen <henrik.mikkelsen@ppmanager.dk>
2017-11-22 20:52:58 +0100

    Add static form group

---
+++ resources/assets/js/components/forms/horizontal/StaticFormGroup.vue
@@ -0,0 +1,15 @@
+<template>
+    <form-group :label="label" :label-width="labelWidth" :input-width="inputWidth">
+        <p class="form-control-static">{{ text }}</p>
+    </form-group>
+</template>
+
+<script>
+    import FormGroup from "./FormGroup";
+
+    export default {
+        components: {FormGroup},
+
+        props: ["text", "label", "labelWidth", "inputWidth"]
+    }
+</script>
