188bad0a965fe33c516053c5532907bb9ff6408d
Henrik Mikkelsen <henrik.mikkelsen@ppmanager.dk>
2018-09-05 10:53:34 +0200

    Add CKEditor 5

---
+++ resources/assets/js/components/FormGroups/Horizontal/CkEditor.vue
@@ -0,0 +1,37 @@
+<template>
+    <form-group
+            :name="name"
+            :label="label"
+            :help-text="helpText"
+            :error="error"
+            :label-width="labelWidth"
+            :input-width="inputWidth"
+    >
+        <ck-editor
+                :id="name"
+                :value="value"
+                :toolbar="toolbar"
+                @input="$emit('input', $event)"
+        ></ck-editor>
+    </form-group>
+</template>
+
+<script>
+    import CkEditor from '../Widgets/CkEditor'
+    import FormGroup from "./FormGroup";
+
+    export default {
+        components: {CkEditor, FormGroup},
+
+        props: {
+            "name": {},
+            "label": {},
+            "helpText": {"default": null},
+            "value": {},
+            "error": {},
+            "labelWidth": {},
+            "inputWidth": {},
+            "toolbar": {}
+        }
+    }
+</script>
---
+++ resources/assets/js/components/FormGroups/Widgets/CkEditor.vue
@@ -0,0 +1,29 @@
+<template>
+    <textarea :id="name" class="form-control">{{ value }}</textarea>
+</template>
+
+<script>
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+    export default {
+        props: {
+            "name": {},
+            "value": {},
+            "toolbar": {
+                "default": () => ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', 'undo', 'redo']
+            }
+        },
+
+        mounted() {
+            ClassicEditor
+                .create(this.$el, {
+                    toolbar: this.toolbar,
+                })
+                .then(editor => {
+                    editor.model.document.on('change:data', () => {
+                        this.$emit('input', editor.getData())
+                    })
+                })
+        }
+    }
+</script>
